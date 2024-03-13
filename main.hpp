71b480cc9f2ac057750d2fca4fa1cdeb7cce58c6
hplaRno1 <156960009+hplaRno1@users.noreply.github.com>
2024-03-13 21:53:32 +0000

    Assignment 6-2

--- main.hpp
+++ main.hpp
@@ -16,10 +16,50 @@ int getRandom(void)
         /***************************************************
          * Code your program here
          ***************************************************/
+         int result;
+         srand(time(0));
+         int rdnum1 = rand()%(99 + 1);
+         cout << "Random number #1: " << rdnum1 << endl;
+         int rdnum2 = rand()%(99 + 1);
+         cout << "Random number #2: " << rdnum2 << endl;
+         int rdnum3 = rand()%(99 + 1);
+         cout << "Random number #3: " << rdnum3 << endl;
+        //  int result = getClosestMed(rdnum1, rdnum2, rdnum3);
+        float Mean = (rdnum1 + rdnum2 + rdnum3) / 3.0;
+        cout << "Mean: " << Mean << endl;
+        Mean = round(Mean);
+        cout << "Rounded Mean: " << Mean << endl;
+        int j = 0;
+        while(j != Mean){
+                if (j = Mean){
+                        j = result;
+                        break;
+                }
+                else{
+                        j += 1;
+                }
+        }
+        //  for(int i = 0; i <= Mean; i++){
+        //         if(i = Mean && i != 0){
+        //                 i = result;
+        //                 break;
+        //         }
+        // }
+         cout << "Result: " << result << endl;
+         return result;
 }
-int getClosestMed(int rdnum1, int rdnum2, int rdnum3)
-{
-        /***************************************************
-         * Code your program here
-         ***************************************************/
-}
+// int getClosestMed(int rdnum1, int rdnum2, int rdnum3)
+// {
+//         /***************************************************
+//          * Code your program here
+//          ***************************************************/
+//          int result;
+//          float Mean = (rdnum1 + rdnum2 + rdnum3) / 3.0;
+//          for(float i = 0; i <= Mean; i++){
+//                 if(i = Mean){
+//                         i = result;
+//                         break;
+//                 }
+//         }
+//          return result;
+// }
